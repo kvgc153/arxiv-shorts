04b99f39a448d265fc049254538706be863b9fac
keerthi <kvch@ucdavis.edu>
2023-12-30 21:46:33 -0800

    v2.0.2

--- README.md
+++ README.md
@@ -6,27 +6,22 @@ Download the extension from the [Firefox addons store](https://addons.mozilla.or
 
 
 ## Demo
-![](arxiv-shorts-demo3.gif)
+![](arxiv-shorts-demo.gif)
 
 
 ## Features
-This extension adds three key components to each arxiv paper:
+This extension adds key components to each arxiv paper:
 - An AI generated summary of the abstract
 - A list of concepts needed to understand the abstract, including a brief explanation of their importance and how they are used in the abstract.
-- Other relevant papers to read.
+- Other relevant papers to read (experimental).
 - Load arxiv pdf on the same page
 - Search for astrobites articles that cover the paper.
 - Annotate pdfs using hypothes.is
 
-You can try this extension by visiting any article on astro-ph:GA after October 5, 2023*.
-
-Click [here](features.md) for a detailed documentation of the features.
-
+Example astro-ph:GA article to try out this extension : https://arxiv.org/abs/2306.03120v1
 
 ### Acknowledgements 
-Thank you to arXiv and ADS for use of its open access interoperability
-
-
+Thank you to arXiv for use of its open access interoperability
 
 
 ## Support and development
--- extension/src/background.js
+++ extension/src/background.js
@@ -1,159 +1,130 @@
 if(window['visited'] != 'true'){
 
-window['visited'] = 'true';
+  window['visited'] = 'true';
 
-var source = document.getElementById('content');
-source.innerHTML +=DOMPurify.sanitize(`
- <div  id="summaryBox" style="left: 70%; height: 70%; position: fixed; width: 28%; bottom: 1%;z-index:10; background-color:transparent; overflow: auto;">
-    <div class="card" style="width: 100%;">
-      <div class="card-body">
-           <div id="summarizedNotes" class="card-text" >
-           </div>
-      </div>
-    </div>
- </div>`);
+  var source = document.getElementsByClassName("abstract")[0];
+  var urlSplit = window.location.toString().split('//')[1];
+  var urlSplit = urlSplit.split('/');
+  var arxivID  = urlSplit[urlSplit.length-1];
+  var arxivTitle = document.getElementsByClassName("title mathjax")[0].outerText;
 
-var urlSplit = window.location.toString().split('//')[1];
-var urlSplit = urlSplit.split('/');
-var arxivID  = urlSplit[urlSplit.length-1];
+  
+  var acceptTokens = ["v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8"];
+  var tokenFlag = false;
+  for (var i=0; i<acceptTokens.length; i++){
+    if(arxivID.includes(acceptTokens[i])){
+      tokenFlag = true;
+    }
+  }
+  if(tokenFlag == false){
+    // get element id from page if token is not present
+    var newURL = document.getElementsByClassName('tablecell arxividv')[0].getElementsByClassName("arxivid")[0].firstElementChild.getAttribute("href");
+    var urlSplit = newURL.split('//')[1];
+    var urlSplit = urlSplit.split('/');
+    var arxivID  = urlSplit[urlSplit.length-1];   
+  }
 
+  var summaryJSONurl = "https://www.cosmicnoon.com/cgi-bin/arxivshorts.cgi?id=" + arxivID + "&token=" + arxivToken;
 
-var summaryJSONurl = "https://www.cosmicnoon.com/cgi-bin/arxivshorts.cgi?id=" + arxivID + "&token=" + arxivToken;
-var summaryOnlyJSON = 'https://raw.githubusercontent.com/kvgc/arxiv-shorts/main/arxiv-shorts.json';
+  function checkURL(url){
+    // returns boolean
+    return(window.location.href.indexOf(url) > -1)
+  }
 
-function checkURL(url){
-  // returns boolean
-  return(window.location.href.indexOf(url) > -1)
-}
-
-async function returnJSON(url, mode) {
-  const response = await fetch(url);
-  const summaryJSON = await response.json();
-
-  if(mode == 'abstract'){
-    try{
-      if(summaryJSON.hasOwnProperty('summary')){
-        var makeHTML0 = "<h3> Summary: </h3>" + summaryJSON['summary'] + "<br><br><hr> <h3> Concepts: </h3><br><hr>";
-        document.getElementById('summarizedNotes').innerHTML += DOMPurify.sanitize(makeHTML0 );
-    
-        var concepts = summaryJSON['GPT']['concepts'];
-        var conceptsKeys = Object.keys(concepts);
-    
-        var makeHTML1 = ''
-        for(var j=0; j< conceptsKeys.length; j++){
-          var foo = j+1;
-          makeHTML1 += `
-          <div class="card bg-light mb-3">
-            <div class="card-body">
-              <h5 class="card-title">${foo}. ${conceptsKeys[j]}</h5><br>
-              <p class="card-text">
-                ${concepts[conceptsKeys[j]].context}<br><br>
-                <b>Def'n: </b>${concepts[conceptsKeys[j]].definition}<br>
-                <b>Usage: </b>${concepts[conceptsKeys[j]].usefulness}<br>
-              </p>
+  async function returnJSON(url, mode) {
+    source.innerHTML +=DOMPurify.sanitize(`
+    <br><br>
+  <div  id="summaryBox" style="background-color:transparent; overflow: auto;">
+      <div class="card" style="width: 100%;">
+        <div class="card-body">
+            <div id="summarizedNotes" class="card-text" >
             </div>
-          </div><br><br>
-        `;
-        }
-        document.getElementById('summarizedNotes').innerHTML += DOMPurify.sanitize(makeHTML1);
-    
-    
-    
-    
-    
-    
-    
-        var similarPapers = summaryJSON['ADS']['similar'];
-        document.getElementById('summarizedNotes').innerHTML += DOMPurify.sanitize("<h3> Related papers: </h3><ul>");
-        for (let idx = 0; idx < similarPapers.length; idx++) {
-    
-          const foo = document.createElement('a');
-          foo.setAttribute('href', "https://ui.adsabs.harvard.edu/abs/"+ similarPapers[idx]['bibcode']);
-          foo.textContent = similarPapers[idx]['title'][0];
-          document.getElementById('summarizedNotes').innerHTML += DOMPurify.sanitize("<li>"+ foo.outerHTML + "</li>");
-        }
-        document.getElementById('summarizedNotes').innerHTML += "</ul>";
-      }
-      else{
-        var similarPapers = summaryJSON['ADS']['similar'];
-        document.getElementById('summarizedNotes').innerHTML += DOMPurify.sanitize("<h3> Related papers: </h3><br>");
-        for (let idx = 0; idx < similarPapers.length; idx++) {
-    
-          const foo = document.createElement('a');
-          foo.setAttribute('href', "https://ui.adsabs.harvard.edu/abs/"+ similarPapers[idx]['bibcode']);
-          foo.textContent = similarPapers[idx]['title'][0];
-          document.getElementById('summarizedNotes').innerHTML += DOMPurify.sanitize(foo.outerHTML + "<br><br>");
-        } 
-      }
-      }
-      catch(err){
-        console.log(err); 
-      }
-      ////////////////////////////////////
-      // Hide notes panel if not needed //
-      ////////////////////////////////////
-      if(document.getElementById('summarizedNotes').innerHTML.length < 15){
-        // check if anything was written to the HTML block and if not hide it 
-        document.getElementById('summaryBox').style.opacity = '0';
-      }
-  }
-  else if(mode == 'weekpage'){
-    ///////////////////////////////////////////////
-    ///// Run when user visits past week page ///// 
-    ///////////////////////////////////////////////
-    if(true){    
-      var papers = document.getElementsByClassName("list-identifier");
-      var papersMeta = document.getElementsByClassName("meta"); 
-      // len(papers) == len(papersMeta)
-      for(var i =0; i< papers.length;i++){
-        var currentPageUrl = "http://"+ papers[i].childNodes[0].href.split('//')[1];
-        var versions = ["", "v3", "v2", "v1"]; 
+        </div>
+      </div>
+  </div>`);
 
-        for(var j=0; j< versions.length; j++){      
-          try {
-            var AISummary = summaryJSON[currentPageUrl+versions[j]]['summary'];
+    const response = await fetch(url);
+    const summaryJSON = await response.json();
 
-            var newNode = document.createElement("div");
-            newNode.innerHTML               =  DOMPurify.sanitize("<b> Generated Summary: </b><br>" + AISummary + "<br>");
-            newNode.style.border            = "2px dotted black";
-            newNode.style.padding           = "10px";
-            newNode.style.backgroundColor   = "#F0F8FF";
-            newNode.style.width             = "40%";
-            newNode.style.fontFamily        = "Cambria";
-            papersMeta[i].childNodes[3].appendChild(newNode);
-            // papers1[0].childNodes[3]
+    if(mode == 'abstract'){
+      try{
+        if(summaryJSON.hasOwnProperty('summary')){
+          var makeHTML0 = "<h5> Summary: </h5>" + summaryJSON['summary'] +  "<br><br><hr> <h5> Concepts: </h5><br><hr>";
+          document.getElementById('summarizedNotes').innerHTML += DOMPurify.sanitize(makeHTML0 );
+      
+          var concepts = summaryJSON['GPT']['concepts'];
+          var conceptsKeys = Object.keys(concepts);
+      
+          var makeHTML1 = ''
+          for(var j=0; j< conceptsKeys.length; j++){
+            var foo = j+1;
+            relatedPapers = '';
+            // for(var k=0;k<concepts[conceptsKeys[j]].ss_id.length;k++){
+            myKeys ={arxivTitle: 'true'};
+            for(var k=0;k<4;k++){
+              if(myKeys.hasOwnProperty(concepts[conceptsKeys[j]].ss_title[k])){;}
+              else{
+                if(arxivTitle != concepts[conceptsKeys[j]].ss_title[k]){
+                myKeys[concepts[conceptsKeys[j]].ss_title[k]] = 'true';
+                relatedPapers += `<a href="${concepts[conceptsKeys[j]].ss_id[k]}">${concepts[conceptsKeys[j]].ss_title[k]}</a><br> `;
+                }
+              }
+            }
+            makeHTML1 += `
+            <div class="card bg-light mb-3">
+              <div class="card-body">
+                <h5 class="card-title">${foo}. ${conceptsKeys[j]}</h5>
+                <p class="card-text">
+                  ${concepts[conceptsKeys[j]].definition}.${concepts[conceptsKeys[j]].usefulness} <br><br>
+                   <b>Learn more: </b> 
+                    <a href="https://en.wikipedia.org/w/index.php?fulltext=1&search=${conceptsKeys[j]}" class="btn btn-outline-primary btn-sm"> Wikipedia</a>
+                    <a href="https://astrobites.org/?s=${conceptsKeys[j]}" class="btn btn-outline-primary btn-sm"> Astrobites</a>
+                    <a href="https://www.google.com/search?q=${conceptsKeys[j]}" class="btn btn-outline-primary btn-sm"> Google</a>
+                    <a href="https://www.bing.com/search?showconv=1&sendquery=1&q= tell me about ${conceptsKeys[j]}. Suggest me useful articles to read about it. Use images from the internet as required" class="btn btn-outline-primary btn-sm"> Bing AI Chat</a>
 
-            
-          } catch (error) {
-            console.error(error);
+                   <br><br>
+                  <b>Related arXiv papers:</b><br> ${relatedPapers}<br>
+                </p>
+              </div>
+            </div>
+          `;
           }
+          document.getElementById('summarizedNotes').innerHTML += DOMPurify.sanitize(makeHTML1);
+
+    
+          // var similarPapers = summaryJSON['ADS']['similar'];
+          // document.getElementById('summarizedNotes').innerHTML += DOMPurify.sanitize("<h5> Similiar papers: </h5><ul>");
+          // for (let idx = 0; idx < similarPapers.length; idx++) {
+      
+          //   const foo = document.createElement('a');
+          //   foo.setAttribute('href', "https://ui.adsabs.harvard.edu/abs/"+ similarPapers[idx]['bibcode']);
+          //   foo.textContent = similarPapers[idx]['title'][0];
+          //   document.getElementById('summarizedNotes').innerHTML += DOMPurify.sanitize("<li>"+ foo.outerHTML + "</li>");
+          // }
+          // document.getElementById('summarizedNotes').innerHTML += "</ul>";
+
+          }
+
         }
-      }
+        catch(err){
+          console.log(err); 
+        }
+        ////////////////////////////////////
+        // Hide notes panel if not needed //
+        ////////////////////////////////////
+        if(document.getElementById('summarizedNotes').innerHTML.length < 15){
+          // check if anything was written to the HTML block and if not hide it 
+          document.getElementById('summaryBox').style.opacity = '0';
+        }
     }
 
-      ////////////////////////////////////
-    // Hide notes panel if not needed //
-    ////////////////////////////////////
-    if(document.getElementById('summarizedNotes').innerHTML.length < 15){
-      // check if anything was written to the HTML block and if not hide it 
-      document.getElementById('summaryBox').style.opacity = '0';  
-      document.getElementById('togglerMain').style.opacity = '0';
     }
+
+    if(checkURL("https://arxiv.org/abs/")){
+    returnJSON(summaryJSONurl, 'abstract'); 
   }
 
 
-
-
-  }
-
-  if(checkURL("https://arxiv.org/abs/")){
-  returnJSON(summaryJSONurl, 'abstract'); 
-}
-
-
-if(checkURL("https://arxiv.org/list/astro-ph.GA/pastweek?")| checkURL("https://arxiv.org/list/astro-ph.GA/recent") | checkURL("https://arxiv.org/list/astro-ph.GA/new")) {
-  returnJSON(summaryOnlyJSON, 'weekpage'); 
-}
 
 }
 
