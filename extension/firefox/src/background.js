3427ffc65d04301fb52f06c41cd33b4ecda56b0a
Keerthi <kvch153@gmail.com>
2024-10-20 15:36:41 -0700

    new interface

--- extension/firefox/src/background.js
+++ extension/firefox/src/background.js
@@ -9,22 +9,18 @@ if(window['visited'] != 'true'){
   var arxivTitle = document.getElementsByClassName("title mathjax")[0].outerText;
 
   
-  var acceptTokens = ["v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8"];
-  var tokenFlag = false;
-  for (var i=0; i<acceptTokens.length; i++){
-    if(arxivID.includes(acceptTokens[i])){
-      tokenFlag = true;
+  var invalidTokens = ["v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8"];
+  // if arxivID contains any of the tokens, then we have to santize the url 
+  for(var i=0; i<invalidTokens.length; i++){
+    if(arxivID.includes(invalidTokens[i])){
+      var arxivID = arxivID.split(invalidTokens[i])[0];
     }
   }
-  if(tokenFlag == false){
-    // get element id from page if token is not present
-    var newURL = document.getElementsByClassName('tablecell arxividv')[0].getElementsByClassName("arxivid")[0].firstElementChild.getAttribute("href");
-    var urlSplit = newURL.split('//')[1];
-    var urlSplit = urlSplit.split('/');
-    var arxivID  = urlSplit[urlSplit.length-1];   
-  }
+
+  var arxivID = arxivID.replace(".","_")
 
   var summaryJSONurl = "https://www.cosmicnoon.com/cgi-bin/arxivshorts.cgi?id=" + arxivID + "&token=" + arxivToken;
+  console.log(summaryJSONurl);
 
   function checkURL(url){
     // returns boolean
@@ -32,16 +28,16 @@ if(window['visited'] != 'true'){
   }
 
   async function returnJSON(url, mode) {
-    source.innerHTML +=DOMPurify.sanitize(`
-    <br><br>
-  <div  id="summaryBox" style="background-color:transparent; overflow: auto;">
-      <div class="card" style="width: 100%;">
-        <div class="card-body">
-            <div id="summarizedNotes" class="card-text" >
-            </div>
-        </div>
-      </div>
-  </div>`);
+    source.innerHTML += DOMPurify.sanitize(`
+<br><br>
+<div id="summaryBox" style="background-color:transparent; overflow: auto; padding: 16px;">
+  <div style="border: 1px solid #e1e8ed; box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12); border-radius: 10px; max-width: 600px; margin: 0 auto; padding: 16px; background-color: #ffffff;">
+    <div style="font-family: Arial, sans-serif;">
+      <h5 style="font-size: 16px; margin-bottom: 8px; color: #1da1f2;">Summary</h5>
+      <div id="summarizedNotes" style="font-size: 14px; color: #14171a;"></div>
+    </div>
+  </div>
+</div>`);
 
     const response = await fetch(url);
     const summaryJSON = await response.json();
@@ -49,58 +45,20 @@ if(window['visited'] != 'true'){
     if(mode == 'abstract'){
       try{
         if(summaryJSON.hasOwnProperty('summary')){
-          var makeHTML0 = "<h5> Summary: </h5>" + summaryJSON['summary'] +  "<br><br><hr> <h5> Concepts: </h5><br><hr>";
-          document.getElementById('summarizedNotes').innerHTML += DOMPurify.sanitize(makeHTML0 );
-      
-          var concepts = summaryJSON['GPT']['concepts'];
-          var conceptsKeys = Object.keys(concepts);
-      
-          var makeHTML1 = ''
-          for(var j=0; j< conceptsKeys.length; j++){
-            var foo = j+1;
-            relatedPapers = '';
-            // for(var k=0;k<concepts[conceptsKeys[j]].ss_id.length;k++){
-            myKeys ={arxivTitle: 'true'};
-            // for(var k=0;k<4;k++){
-            //   if(myKeys.hasOwnProperty(concepts[conceptsKeys[j]].ss_title[k])){;}
-            //   else{
-            //     if(arxivTitle != concepts[conceptsKeys[j]].ss_title[k]){
-            //     myKeys[concepts[conceptsKeys[j]].ss_title[k]] = 'true';
-            //     relatedPapers += `<a href="${concepts[conceptsKeys[j]].ss_id[k]}">${concepts[conceptsKeys[j]].ss_title[k]}</a><br> `;
-            //     }
-            //   }
-            // }
-            makeHTML1 += `
-            <div class="card bg-light mb-3">
-              <div class="card-body">
-                <h5 class="card-title">${foo}. ${conceptsKeys[j]}</h5>
-                <p class="card-text">
-                  ${concepts[conceptsKeys[j]].definition}.${concepts[conceptsKeys[j]].usefulness} <br><br>
-                   <b>Learn more: </b> 
-                    <a href="https://en.wikipedia.org/w/index.php?fulltext=1&search=${conceptsKeys[j]}" class="btn btn-outline-primary btn-sm"> Wikipedia</a>
-                    <a href="https://astrobites.org/?s=${conceptsKeys[j]}" class="btn btn-outline-primary btn-sm"> Astrobites</a>
-                    <a href="https://www.google.com/search?q=${conceptsKeys[j]}" class="btn btn-outline-primary btn-sm"> Google</a>
-                    <a href="https://www.bing.com/search?showconv=1&sendquery=1&q= tell me about ${conceptsKeys[j]}. Suggest me useful articles to read about it. Use images from the internet as required" class="btn btn-outline-primary btn-sm"> Bing AI Chat</a>
+            var genSummary = summaryJSON['summary'];
+            genSummary = genSummary.split("\n\n");
+            document.getElementById('summarizedNotes').innerText = genSummary[1] + "\n\n";
 
-                   </p>
-              </div>
-            </div>
-          `;
-          }
-          document.getElementById('summarizedNotes').innerHTML += DOMPurify.sanitize(makeHTML1);
-
-    
-          // var similarPapers = summaryJSON['ADS']['similar'];
-          // document.getElementById('summarizedNotes').innerHTML += DOMPurify.sanitize("<h5> Similiar papers: </h5><ul>");
-          // for (let idx = 0; idx < similarPapers.length; idx++) {
-      
-          //   const foo = document.createElement('a');
-          //   foo.setAttribute('href', "https://ui.adsabs.harvard.edu/abs/"+ similarPapers[idx]['bibcode']);
-          //   foo.textContent = similarPapers[idx]['title'][0];
-          //   document.getElementById('summarizedNotes').innerHTML += DOMPurify.sanitize("<li>"+ foo.outerHTML + "</li>");
-          // }
-          // document.getElementById('summarizedNotes').innerHTML += "</ul>";
-
+            //Show the tags in a different color
+            var tags = genSummary[2];
+            // Make color div 
+            var colorDiv = document.createElement('div');
+            colorDiv.style.color = '#e08848';
+            colorDiv.style.display = 'inline';
+            colorDiv.style.fontSize = '14px';
+            colorDiv.style.fontWeight = 'bold';
+            colorDiv.innerText = tags;
+            document.getElementById('summarizedNotes').innerHTML += DOMPurify.sanitize(colorDiv.outerHTML);
           }
 
         }
@@ -121,8 +79,5 @@ if(window['visited'] != 'true'){
     if(checkURL("https://arxiv.org/abs/")){
     returnJSON(summaryJSONurl, 'abstract'); 
    }
-
-
-
 }
 
